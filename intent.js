2ccf1fa7da39231042696036088fdb0777fd3638
Simon Mölzer <evilhowser@gmail.com>
2016-10-16 21:29:27 +0200

    Add intent.js as an event emitter to handle incoming intents

---
+++ intent.js
@@ -0,0 +1,18 @@
+const EventEmitter = require('events');
+
+// Might be overkill to extend this
+// but it will simplify later if we need to
+// add things.
+class intentEmitter extends EventEmitter {
+
+    // Prints all the intents that have registered listeners
+    listIntents() {
+	console.log("Currently handled intents:");
+	var x = this.eventNames();
+	for (var i in x) {
+	    console.log(x[i]);
+	}
+    }
+}
+
+module.exports = intentEmitter;
--- main.js
+++ main.js
@@ -3,9 +3,24 @@ const config = require('./config');
 const tss = require('./tss');
 const fs = require('fs');
 const voice = require('./voicerecognition');
+const intent = require('./intent');
 
 //var app = api_ai(config.apiai_client_token);
 
 //tss.playText("Created reminder for you!")
 
-voice.start((text) => {console.log(text)});
+//voice.start((text) => {console.log(text)});
+
+testIntent = new intent();
+
+testIntent.on('bedtime', () => {
+    console.log("bedtime event");
+});
+
+testIntent.on('remind', () => {
+    console.log("remind event");
+});
+
+testIntent.listIntents();
+testIntent.emit('bedtime');
+testIntent.emit('remind');
